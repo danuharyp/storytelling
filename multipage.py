6c176ccd651f8930d374e2d7f12214d2f0d54a57
PapaQuon <70670135+anahdraw@users.noreply.github.com>
2024-07-01 05:11:57 +0700

    Add files via upload

--- multipage.py
+++ multipage.py
@@ -2,12 +2,27 @@ import streamlit as st
 
 class MultiPage: 
     def __init__(self) -> None:
-        """Constructor class to store variables."""
-
+        """Constructor to store variables."""
         self.pages = []
     
     def add_page(self, title, func) -> None: 
-        """Class Method to Add pages to the project
+        """Class Method to Add pages.
         Args:
-            title ([str]): The title of page which we are adding to the list of apps 
-            func: Python
+            title ([str]): The title of page to add.
+            func: Python function to render this page.
+        """
+        self.pages.append({
+            "title": title, 
+            "function": func
+        })
+
+    def run(self):
+        # Dropdown to select the page to run  
+        page = st.sidebar.selectbox(
+            'Navigation', 
+            self.pages, 
+            format_func=lambda page: page['title']
+        )
+
+        # Run the selected page
+        page['function']() 
